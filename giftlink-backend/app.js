aa8334013dfdb15e0b37819fd509f4123d2debfb
shuja2 <shuja201234@gmail.com>
2024-11-01 18:30:54 -0400

    gift routes

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
 
+const giftRoutes = require('./routes/giftRoutes');
+
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 
@@ -36,7 +38,7 @@ app.use(pinoHttp({ logger }));
 
 // Use Routes
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 //{{insert code here}}
